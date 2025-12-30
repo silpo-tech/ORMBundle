8eda5fe9d6b2df418c17a6309e14a4f49b481fd6
Volodymyr Melko <v.melko@foodtech.team>
2025-12-30 11:21:28 +0200

    fix(DI): load services.yml

--- src/DependencyInjection/ORMExtension.php
+++ src/DependencyInjection/ORMExtension.php
@@ -6,13 +6,18 @@ namespace ORMBundle\DependencyInjection;
 
 use ORMBundle\Backoff\DefaultBackoffFactory;
 use ORMBundle\DependencyInjection\DBAL\Configuration;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class ORMExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+
         // Override the DBAL configuration class parameter to use our custom Configuration
         $container->setParameter('doctrine.dbal.configuration.class', Configuration::class);
 
